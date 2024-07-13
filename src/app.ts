10ed7be21c16c88cb482346d38940f97f81950ab
hmsmiraz <hmsmiraz64729@gmail.com>
2024-07-13 21:02:41 +0600

    setup

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,17 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import router from './app/routes';
 
 const app: Application = express();
 
 // parsers
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: ['http://localhost:5173'] }));
+// app.use(cors({ origin: ['http://localhost:5173'] }));
 
 // Application routes
-// app.use('/api/v1', router);
+app.use('/', router);
 
 const test = (req: Request, res: Response) => {
   res.send(`Welcome Victory Sports Management System Backend`);
